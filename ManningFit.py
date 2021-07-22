cd7c0cc7c4fdd26bdf11680e80be0eb508177377
Andy Wickert <andy@northernwidget.com>
2021-07-22 18:28:34 -0500

    Add a linear hypsometry

--- ManningFit.py
+++ ManningFit.py
@@ -18,7 +18,9 @@ def _manning(h, n, k_Qbank, P_Qbank, stage_depth_Q_offset, h_bank):
     * h: Input. Stage.
     * n: Manning's n
     * k_Qbank, P_Qbank: Power-law fititng parameters for floodplain hypsometry,
-                        thereby controlling flow depth beyond the channel
+                        thereby controlling flow depth beyond the channel, that 
+                        also fold in the value of Manning's n and slope
+                        (k_Qbank) and the 2/3 exponent to h (P_Qbank)
     * stage_depth_Q_offset: Q(h=0), meant to solve for the offset between
                             bed elevation and flow stage
     * h_bank: Streambank elevation. This might be known a priori, but it can
@@ -30,11 +32,31 @@ def _manning(h, n, k_Qbank, P_Qbank, stage_depth_Q_offset, h_bank):
     h_fp = _ob * k_Qbank * (h-h_bank)**(P_Qbank * _ob)
     return h_ch + h_fp + stage_depth_Q_offset
 
+def _manning_fp_lin_hypsometry(h, n, k_Qbank, stage_depth_Q_offset, h_bank):
+    """
+    Returns discharge given flow depth, 
+    * h: Input. Stage.
+    * n: Manning's n
+    * k_Qbank: Parameter that lumps floodplain hypsometry ( B(h) ), slope, and 
+               Manning's n for the floodplain together.
+    * stage_depth_Q_offset: Q(h=0), meant to solve for the offset between
+                            bed elevation and flow stage
+    * h_bank: Streambank elevation. This might be known a priori, but it can
+              also be solved here as a function of the inflection in the
+              rating-curve data
+    """
+    h_ch = b * h**(5/3.) * S**(1/2.) / n
+    _ob = (h > h_bank)
+    h_fp = _ob * k_Qbank * (h-h_bank)**(2/3.)
+    return h_ch + h_fp + stage_depth_Q_offset
+
 popt, pcov = curve_fit( _manning, data['Stage'], data['Q'] )
+
+popt, pcov = curve_fit( _manning_fp_lin_hypsometry, data['Stage'], data['Q'] )
 
 _h = np.arange(0.,10.1, 0.1)
 plt.plot(data['Stage'], data['Q'], 'k.')
-plt.plot(_h, _manning(_h, *popt))
+plt.plot(_h, _manning_fp_lin_hypsometry(_h, *popt))
 b *= 2
-plt.plot(_h, _manning(_h, *popt))
+plt.plot(_h, _manning_fp_lin_hypsometry(_h, *popt))
 
