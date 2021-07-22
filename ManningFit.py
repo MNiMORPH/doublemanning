66ce2a3d0b7bf0357c43c35106df05b9ee428bfa
Andy Wickert <andy@northernwidget.com>
2021-07-22 17:45:47 -0500

    Split components and comment _manning function

--- ManningFit.py
+++ ManningFit.py
@@ -13,34 +13,30 @@ data['Stage'] /= 3.28
 b = 70. # meters
 S = 1E-4
 
-"""
-def _manning(h, k_Qbank, P_Qbank, stage_depth_Q_offset):
-    Q_ch = b * h**(5/3.) * S**(1/2.)
-    if h > h_bank:
-        Q_ob = k_Qbank * (h - h_bank)**(P_Qbank)
-    else:
-        Q_ob = 0
-    return Q_ch + Q_ob + stage_depth_Q_offset
-"""
-
 def _manning(h, n, k_Qbank, P_Qbank, stage_depth_Q_offset, h_bank):
-    return ( b * h**(5/3.) * S**(1/2.) / n ) \
-           + (h > h_bank) * k_Qbank * (h - h_bank)**(P_Qbank * (h > h_bank)) \
-           + stage_depth_Q_offset
+    """
+    Returns discharge given flow depth, 
+    * h: Input. Stage.
+    * n: Manning's n
+    * k_Qbank, P_Qbank: Power-law fititng parameters for floodplain hypsometry,
+                        thereby controlling flow depth beyond the channel
+    * stage_depth_Q_offset: Q(h=0), meant to solve for the offset between
+                            bed elevation and flow stage
+    * h_bank: Streambank elevation. This might be known a priori, but it can
+              also be solved here as a function of the inflection in the
+              rating-curve data
+    """
+    h_ch = b * h**(5/3.) * S**(1/2.) / n
+    _ob = (h > h_bank)
+    h_fp = _ob * k_Qbank * _ob**(P_Qbank * _ob)
+    return h_ch + h_fp + stage_depth_Q_offset
 
 #_h = np.arange(0.,10.)
 #_Q = _manning(_h, 1E2, 8/3., 0.)
 
 #plt.plot(_Q, _h, 'k.')
 
-popt, pcov = curve_fit( _manning, data['Stage'], data['Q'] )#,
-#                        p0 = [0.025, 1E4, 8/3., 0., 1.],
-#                        bounds=[[1E-5, 0., 5/3., -500., 1E-3],
-#                                [1., 1E8, 10., 500., 1E2]] )
-popt, pcov = curve_fit( _manning, data['Stage'], data['Q'],
-                        p0 = [0.025, 1E4, 8/3., 0., 1.],
-                        bounds=[[1E-5, 0., 8/3., -500., 1E-3],
-                                [1., 1E8, 10., 500., 1E2]] )
+popt, pcov = curve_fit( _manning, data['Stage'], data['Q'] )
 
 _h = np.arange(0.,10.1, 0.1)
 plt.plot(data['Stage'], data['Q'], 'k.')
