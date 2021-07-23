5684e1cada92426c003fe42928c893c7edf63dda
Andy Wickert <andy@northernwidget.com>
2021-07-22 19:03:29 -0500

    Really calculating discharge!

--- ManningFit.py
+++ ManningFit.py
@@ -27,10 +27,10 @@ def _manning(h, n, k_Qbank, P_Qbank, stage_depth_Q_offset, h_bank):
               also be solved here as a function of the inflection in the
               rating-curve data
     """
-    h_ch = b * h**(5/3.) * S**(1/2.) / n
+    Q_ch = b * h**(5/3.) * S**(1/2.) / n
     _ob = (h > h_bank)
-    h_fp = _ob * k_Qbank * (h-h_bank)**(P_Qbank * _ob)
-    return h_ch + h_fp + stage_depth_Q_offset
+    Q_fp = _ob * k_Qbank * (h-h_bank)**(P_Qbank * _ob)
+    return Q_ch + Q_fp + stage_depth_Q_offset
 
 popt, pcov = curve_fit( _manning, data['Stage'], data['Q'] )
 
